18d29d09fed217eebd66e53dcd456eeefc4d399d
Savil Srivastava <676452+savil@users.noreply.github.com>
2024-04-15 18:12:15 -0700

    hello world 13

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
     "net/http"
 )
 func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "hello world 12\n")
+    fmt.Fprintf(w, "hello world 13\n")
 }
 
 func main() {
