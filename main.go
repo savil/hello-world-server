370cede0e130e7d9863bcef5f6655e40852e36cd
mohsenari <mohsen.ansari@jetpack.io>
2024-04-19 16:52:00 -0400

    Added 500 route

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
+
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
+
 func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "hello world 14\n")
+	fmt.Fprintf(w, "hello world 14\n")
+}
+
+func handle500(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 - Something bad happened!"))
 }
 
 func main() {
-    http.HandleFunc("/hello", hello)
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/500", handle500)
 
-    fmt.Println("Serving requests at port 8080\n")
-    http.ListenAndServe(":8080", nil)
+	fmt.Println("Serving requests at port 8080\n")
+	http.ListenAndServe(":8080", nil)
 }
